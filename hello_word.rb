6b1ad437c2030bf6e6beea31a2b08944423f782b
fargelus <ddraudred@gmail.com>
2020-01-09 12:41:58 +0300

    Hello, Gosu

---
+++ hello_word.rb
@@ -0,0 +1,21 @@
+require 'gosu'
+
+class GameWindow < Gosu::Window
+  def initialize(width=320, height=240, fullscreen=false)
+    super
+    self.caption = 'Hello'
+    @message = Gosu::Image.from_text(
+      self,
+      'Hello world',
+      Gosu.default_font_name,
+      30
+    )
+  end
+
+  def draw
+    @message.draw(10, 10, 0)
+  end
+end
+
+window = GameWindow.new
+window.show
