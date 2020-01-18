756ea5ac6dc3ffc7ba1429d6d9d36ce1a24128e5
fargelus <ddraudred@gmail.com>
2020-01-18 20:17:51 +0300

    Init win

--- lib/space_invaders.rb
+++ lib/space_invaders.rb
@@ -1,6 +1,16 @@
-require "space_invaders/version"
+# frozen_string_literal: true
+
+require 'gosu'
+require_relative 'space_invaders/version'
 
 module SpaceInvaders
-  class Error < StandardError; end
-  # Your code goes here...
+  class Game < Gosu::Window
+    def initialize(width = 640, height = 480)
+      super
+      self.caption = 'Space Invaders'
+    end
+  end
 end
+
+game = SpaceInvaders::Game.new
+game.show
