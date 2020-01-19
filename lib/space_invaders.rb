dc4be7aedcd1400d9bcc2060869cd404fe4c4741
fargelus <ddraudred@gmail.com>
2020-01-19 12:37:46 +0300

    Spaceship move

--- lib/space_invaders.rb
+++ lib/space_invaders.rb
@@ -9,14 +9,45 @@ module SpaceInvaders
                    height = Settings::HEIGHT)
       super
       self.caption = Settings::CAPTION
+      @width = width
+      @height = height
+      @draws = 0
+      @movements = 0
 
       setup_bg
-      @ship = Ship.new(0, 0)
+      setup_assets
+    end
+
+    def update
+      @ship.set(@ship.x - 3, @ship.y) if move_ship_left?
+      @ship.set(@ship.x + 3, @ship.y) if move_ship_right?
+    end
+
+    def move_ship_left?
+      button_down?(Gosu::KbLeft) && @ship.x > 0
+    end
+
+    def move_ship_right?
+      button_down?(Gosu::KbRight) && @ship.x + @ship.w < @width
+    end
+
+    def button_down(id)
+      close if id == Gosu::KbEscape
+      @movements += 1
+    end
+
+    def button_up(_id)
+      @movements -= 1
     end
 
     def draw
+      @draws += 1
       @bg.draw(0, 0, 0)
       @ship.draw
+    end
+
+    def needs_redraw?
+      @draws == 0 || @movements > 0
     end
 
     private
@@ -24,6 +55,12 @@ module SpaceInvaders
     def setup_bg
       bg_image = Settings.images_path('space.png')
       @bg = Gosu::Image.new(bg_image)
+    end
+
+    def setup_assets
+      @ship = Ship.new
+      @ship.set(@width / 2 - @ship.w / 2,
+                @height * 0.9 - @ship.h / 2)
     end
   end
 end
--- lib/space_invaders/ship.rb
+++ lib/space_invaders/ship.rb
@@ -4,11 +4,22 @@ require 'gosu'
 require_relative 'settings'
 
 class Ship
+  attr_reader :x, :y, :width, :height
+  alias w width
+  alias h height
+
   def initialize(x = 0, y = 0)
     @x = x
     @y = y
     @ship_path = SpaceInvaders::Settings.images_path('ship.png')
     @ship = Gosu::Image.new(@ship_path)
+    @width = @ship.width
+    @height = @ship.height
+  end
+
+  def set(x, y)
+    @x = x
+    @y = y
   end
 
   def draw
