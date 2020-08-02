07216b74b9a38510e8982c3d55c3f760f64e1b37
Dmitriy Nemykin <ddraudred@gmail.com>
2020-08-02 11:14:07 +0300

    Basic explosion setup

--- lib/space_invaders/base/settings.rb
+++ lib/space_invaders/base/settings.rb
@@ -56,5 +56,6 @@ module SpaceInvaders
     RED_COLOR = Gosu::Color.new(214, 17, 17)
     GREEN_COLOR = Gosu::Color.new(9, 222, 1)
     SUNNY_COLOR = Gosu::Color.new(217, 240, 8)
+    EFFECTS_DIR = IMAGES_PATH / 'effects'
   end
 end
